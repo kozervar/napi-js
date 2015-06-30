28581198c6462c63433152050b54b5bf5b6b31c1
Michał Budzyński <michal@virtualdesign.pl>
2015-06-30 18:02:59 +0200

    cmd parameters

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
 'use strict';
+
+var napijs = require('./..');
+
+var fileName = process.argv[2];
+
+if (!fileName) {
+  console.log('Not enough parameters. Usage:');
+  console.log('> napijs path/to/file');
+  return;
+}
+napijs.downloadSubtitles(fileName, napijs.LANGUAGE.POLISH).then(function (obj) {
+        console.log(obj);
+    }, function (err) {
+        console.error(err);
+    },
+    function (progress) {
+        console.info(progress);
+    }
+);
